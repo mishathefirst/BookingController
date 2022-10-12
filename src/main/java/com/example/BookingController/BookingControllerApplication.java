1a1c0ac855f8de1ff7d92946db3c72aa0d41cf14
Mikhail Nogovitsyn <mikhail.nogovitsyn2015@yandex.ru>
2022-10-12 20:24:21 +0300

    Initial commit

---
+++ src/main/java/com/example/BookingController/BookingControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookingController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookingController/BookingControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
